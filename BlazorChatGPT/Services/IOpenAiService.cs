f24e881c77d7bc2f2e553a60a855e230191f0e26
Matteo Pagani <matteo.pagani@microsoft.com>
2023-06-07 12:43:03 +0200

    Added new semantic kernel features

--- BlazorChatGPT/Services/IOpenAiService.cs
+++ BlazorChatGPT/Services/IOpenAiService.cs
@@ -4,9 +4,17 @@
     {
         Task<string> GenerateSessionAsync(string topic, string tone);
 
+        Task<string> GenerateSessionInlineAsync(string topic, string tone);
+
         Task<string> GenerateBusinessReportAsync(string customer, string problem, string solution);
 
         Task<string> GenerateBusinessMailAsync(string text);
+
+        Task<string> GenerateTranslatedSessionAsync(string topic, string tone);
+
+        Task<string> GenerateStarWarsMail(string character);
+
+        Task<string> GenerateStarWarsDescriptionAsync(string character);
 
         void UseAzureOpenAI();
         void UseOpenAI();
--- BlazorChatGPT/Services/OpenAiService.cs
+++ BlazorChatGPT/Services/OpenAiService.cs
@@ -1,8 +1,7 @@
-﻿using BlazorChatGPT.Pages;
-using Microsoft.AspNetCore.Components.Forms;
-using Microsoft.SemanticKernel;
-using Microsoft.SemanticKernel.KernelExtensions;
+﻿using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Orchestration;
+using Microsoft.SemanticKernel.Planning;
+using Microsoft.SemanticKernel.SkillDefinition;
 
 namespace BlazorChatGPT.Services
 {
@@ -10,13 +9,12 @@ namespace BlazorChatGPT.Services
     {
         private IKernel kernel;
         private readonly IConfiguration _configuration;
-        private IDictionary<string, ISKFunction> skill;
+        IDictionary<string, ISKFunction> generatorSkills;
+        IDictionary<string, ISKFunction> starWarsSkills;
 
         public OpenAiService(IConfiguration configuration)
         {
             _configuration = configuration;
-
-            kernel = Kernel.Builder.Build();
         }
 
         public void UseOpenAI()
@@ -24,8 +22,13 @@ namespace BlazorChatGPT.Services
             string openAIKey = _configuration["OpenAI:ApiKey"];
             string model = _configuration["OpenAI:Model"];
 
-            kernel.Config.AddOpenAIChatCompletionService(model, model, openAIKey);
-            skill = kernel.ImportSemanticSkillFromDirectory("Skills", "GeneratorSkill");
+            var kernelBuilder = new KernelBuilder();
+            kernelBuilder.WithOpenAIChatCompletionService(model, openAIKey);
+
+            kernel = kernelBuilder.Build();
+
+            generatorSkills = kernel.ImportSemanticSkillFromDirectory("Skills", "GeneratorSkill");
+            starWarsSkills = kernel.ImportSemanticSkillFromDirectory("Skills", "StarWarsSkill");
         }
 
         public void UseAzureOpenAI()
@@ -34,8 +37,37 @@ namespace BlazorChatGPT.Services
             string deploymentName = _configuration["AzureOpenAI:DeploymentName"];
             string endpoint = _configuration["AzureOpenAI:Endpoint"];
 
-            kernel.Config.AddAzureChatCompletionService("chatgpt-azure", deploymentName, endpoint, apiKey, true);
-            skill = kernel.ImportSemanticSkillFromDirectory("Skills", "GeneratorSkill");
+            var kernelBuilder = new KernelBuilder();
+            kernelBuilder.WithAzureChatCompletionService(deploymentName, endpoint, apiKey);
+
+            kernel = kernelBuilder.Build();
+
+            generatorSkills = kernel.ImportSemanticSkillFromDirectory("Skills", "GeneratorSkill");
+            starWarsSkills = kernel.ImportSemanticSkillFromDirectory("Skills", "StarWarsSkill");
+        }
+
+        public async Task<string> GenerateSessionInlineAsync(string topic, string tone)
+        {
+            var context = new ContextVariables();
+            context.Set("topic", topic);
+            context.Set("tone", tone);
+
+            string prompt = @"
+              Generate a title and an abstract for submitting a presentation for a technical conference. Generate the output as a HTML list, with the following format:
+
+            - Title (make it bold): {title} 
+            - Abstract (make it bold): {abstract}
+
+            Use the following tone: {{$tone}}
+
+            The topic of the presentation is defined in the following text enclosed between triple backticks:
+
+            Topic: ```{{$topic}}```  
+            ";
+
+            var function = kernel.CreateSemanticFunction(prompt);
+            var result = await kernel.RunAsync(context, function);
+            return result.Result;
         }
 
         public async Task<string> GenerateSessionAsync(string topic, string tone)
@@ -44,7 +76,7 @@ namespace BlazorChatGPT.Services
             context.Set("topic", topic);
             context.Set("tone", tone);
 
-            var result = await kernel.RunAsync(context, skill["SessionGeneratorFunction"]);
+            var result = await kernel.RunAsync(context, generatorSkills["SessionGeneratorFunction"]);
             if (!result.ErrorOccurred)
             {
                 return result.Result;
@@ -55,6 +87,36 @@ namespace BlazorChatGPT.Services
             }
         }
 
+        public async Task<string> GenerateTranslatedSessionAsync(string topic, string tone)
+        {
+            var context = new ContextVariables();
+            context.Set("topic", topic);
+            context.Set("tone", tone);
+
+            string firstPrompt = @"
+              Generate a title and an abstract for submitting a presentation for a technical conference. Generate the output as a HTML list, with the following format:
+
+            - Title (make it bold): {title} 
+            - Abstract (make it bold): {abstract}
+
+            Use the following tone: {{$tone}}
+
+            The topic of the presentation is defined in the following text enclosed between triple backticks:
+
+            Topic: ```{{$topic}}```  
+            ";
+
+            string secondPrompt = @"
+                Translate the following content in Italian: {{$input}}
+            ";
+
+            var myFirstPromptFunction = kernel.CreateSemanticFunction(firstPrompt);
+            var mySecondPromptFunction = kernel.CreateSemanticFunction(secondPrompt);
+
+            var output = await kernel.RunAsync(context, myFirstPromptFunction, mySecondPromptFunction);
+            return output.Result;
+        }
+
         public async Task<string> GenerateBusinessReportAsync(string customer, string problem, string solution)
         {
             var context = new ContextVariables();
@@ -62,7 +124,7 @@ namespace BlazorChatGPT.Services
             context.Set("problem", problem);
             context.Set("solution", solution);
 
-            var result = await kernel.RunAsync(context, skill["ReportGeneratorFunction"]);
+            var result = await kernel.RunAsync(context, generatorSkills["ReportGeneratorFunction"]);
 
             if (!result.ErrorOccurred)
             {
@@ -76,10 +138,7 @@ namespace BlazorChatGPT.Services
 
         public async Task<string> GenerateBusinessMailAsync(string text)
         {
-            var context = new ContextVariables();
-            context.Set("text", text);
-
-            var result = await kernel.RunAsync(context, skill["MailGeneratorFunction"]);
+            var result = await kernel.RunAsync(text, generatorSkills["MailGeneratorFunction"]);
 
             if (!result.ErrorOccurred)
             {
@@ -89,6 +148,34 @@ namespace BlazorChatGPT.Services
             {
                 return result.LastErrorDescription;
             }
+        }
+
+        public async Task<string> GenerateStarWarsDescriptionAsync(string character)
+        {
+            var result = await kernel.RunAsync(character, starWarsSkills["StarWarsFunction"]);
+
+            if (!result.ErrorOccurred)
+            {
+                return result.Result;
+            }
+            else
+            {
+                return result.LastErrorDescription;
+            }
+        }
+
+        public async Task<string> GenerateStarWarsMail(string character)
+        {
+            var planner = new SequentialPlanner(kernel);
+            var ask = "Write a business mail to describe the following Star Wars character to a customer: {{$input}}";
+            var originalPlan = await planner.CreatePlanAsync(ask);
+
+            var originalPlanResult = await kernel.RunAsync(character, originalPlan);
+
+            Console.WriteLine("Original Plan results:\n");
+            Console.WriteLine(originalPlanResult.Result);
+
+            return originalPlanResult.Result;
         }
     }
 }
